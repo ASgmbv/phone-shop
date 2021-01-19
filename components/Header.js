552762085cce33d4ce766f4b30c56a52fd6344a4
ASgmbv <alimbeksagymbaev@gmail.com>
2021-01-19 10:49:23 +0600

    header

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,22 @@
-import { Box, Container, Flex, Link, Text } from "@chakra-ui/react";
+import { Icon, PhoneIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Container,
+  Flex,
+  HStack,
+  Img,
+  Link,
+  Text,
+} from "@chakra-ui/react";
 import NextLink from "next/link";
 import React from "react";
+
+import {
+  FaFacebook,
+  FaInstagram,
+  FaTelegram,
+  FaWhatsapp,
+} from "react-icons/fa";
 
 const Header = (props) => {
   return (
@@ -15,15 +31,40 @@ const Header = (props) => {
       border="0 solid #d2d6dc"
       {...props}
     >
+      <Flex bg="gray.500" color="white">
+        <Container maxW="4xl" py="1">
+          <Flex justifyContent="space-between">
+            <Flex as="nav" alignItems="center" flexDirection={["row"]}>
+              <PhoneIcon boxSize="4" mr="2" />
+              <Icon as={FaWhatsapp} boxSize="4" mr="2" />
+              <Icon as={FaTelegram} boxSize="4" mr="2" />
+              <Link href="tel:+905525164080" isExternal fontSize="xs">
+                +90 552 516 4080
+              </Link>
+              <Link href="tel:+905372754195" isExternal fontSize="xs" ml="2">
+                +90 537 275 4195
+              </Link>
+            </Flex>
+            <Link
+              isExternal
+              href="https://mobile.donanimhaber.com/profil/2549127"
+            >
+              <Img src="/dhw.png" height="4" />
+            </Link>
+          </Flex>
+        </Container>
+      </Flex>
       <Container maxW="4xl">
-        <Flex py="5" justifyContent="space-between" alignItems="center">
+        <Flex py="2" justifyContent="space-between" alignItems="center">
           <NextLink href="/" passHref>
             <Link fontSize="xl" fontWeight="bold">
-              SJ
+              HG iletişim
             </Link>
           </NextLink>
           <NextLink href="/blog" passHref>
-            <Link fontWeight="500">Blog</Link>
+            <Link fontWeight="500" fontSize="sm" letterSpacing="wider">
+              BLOG
+            </Link>
           </NextLink>
         </Flex>
       </Container>
